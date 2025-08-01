1944943fb669a69fd085ce74d07b8d18bc4521f4
criscuolosubidu <criscuolosubidu@gmail.com>
2025-08-01 14:10:20 +0800

    [modify] 增加说话的语速

--- ttsreal.py
+++ ttsreal.py
@@ -342,7 +342,8 @@ class SovitsTTS(BaseTTS):
             'prompt_text':reftext,
             'prompt_lang':language,
             'media_type':'ogg',
-            'streaming_mode':True
+            'streaming_mode':True,
+            'speed_factor': 1.2
         }
         # req["text"] = text
         # req["text_language"] = language
